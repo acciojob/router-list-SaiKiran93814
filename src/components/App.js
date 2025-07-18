66c2b63f0eb2d370fbbac88c47473f59e635fce5
SaiKiran93814 <teppali123@gmail.com>
2025-07-18 17:08:09 +0530

    Change arror

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-
 import React from "react";
-import './../styles/App.css';
+import { Routes, Route } from "react-router-dom";
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetails";
+
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<ItemList />} />
+      <Route path="/items/:id" element={<ItemDetail />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
